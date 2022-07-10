9ec992545b29a3beac83549fcb55e613ff1e803f
nitred <nitish.k.reddy@gmail.com>
2022-07-10 11:14:19 +0200

    Bugfix: Using poetry for package and dependency management to resolve dependency bugs #3

---
+++ nr_wg_mtu_finder/main_heatmap.py
@@ -0,0 +1,74 @@
+import argparse
+import signal
+import sys
+import time
+from distutils.util import strtobool
+
+from pydantic import BaseModel, StrictInt, StrictStr, root_validator
+from typing_extensions import Literal
+
+from nr_wg_mtu_finder.plot import create_heatmap_from_log
+
+from .mtu_finder import MTUFinder
+
+
+def signal_handler(sig, frame):
+    """Handle ctrl+c interrupt.
+
+    Without this handler, everytime a ctrl+c interrupt is received, the server shutdowns
+    and proceeds to the next iteration in the loop rather than exiting the program
+    altogether.
+    """
+    print("************Received CTRL-C. Will exit in 1 second************")
+    time.sleep(1)
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, signal_handler)
+
+
+class ArgsModel(BaseModel):
+    log_filepath: StrictStr
+    heatmap_filepath: StrictStr
+
+    class Config:
+        orm_mode = True
+
+
+def setup_args():
+    """Setup args."""
+    parser = argparse.ArgumentParser(
+        description=(
+            "nr-wg-mtu-finder-plot - "
+            "Generate a heatmap file (png) from a log file (csv) that was created "
+            "by the `nr-wg-mtu-finder` script. This is useful in case the original "
+            "script file crashed midway."
+        )
+    )
+    parser.add_argument(
+        "--log-filepath",
+        help=(
+            "Absolute path to the log file (csv) that was created by the "
+            "`nr-wg-mtu-finder` script."
+        ),
+        required=True,
+    )
+    parser.add_argument(
+        "--heatmap-filepath",
+        help=(
+            "Absolute path to the heatmap file (png) which will be created from the "
+            "log file (csv)."
+        ),
+        required=True,
+    )
+    args = parser.parse_args()
+    return args
+
+
+def run():
+    args = setup_args()
+    args = ArgsModel.from_orm(args)
+
+    create_heatmap_from_log(
+        log_filepath=args.log_filepath, heatmap_filepath=args.heatmap_filepath
+    )
